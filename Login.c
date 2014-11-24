3be4f54c0b0d98756a7cf006ed835786423fa663
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2014-11-24 16:18:56 -0500

    Finished majority of Login.c. TODO: final linking & error pages

--- Login.c
+++ Login.c
@@ -21,9 +21,60 @@ int valid(char *name, char *password, char*line){
     else return 0;
 }
 
+char** parsePostRequest(){
+    char *len_ = getenv("CONTENT_LENGTH");
+    if (!len_){
+        // invalid post request
+        exit(1);
+    }
+
+
+
+    long len = strtol(len_, NULL, 10);
+    char *postdata = malloc(len + 1);
+    if (!postdata) {
+        // TODO: error page...
+    }
+    fgets(postdata, len + 1, stdin);
+
+    char ** ret = malloc(2*sizeof(char*));
+
+    char *sep1 = strchr(postdata, '&');
+    if (!sep1){
+        // invalid input - not enough args
+    }
+    *sep1 = '\0';
+    sep1 += 1;
+    char *username = strchr(postdata, '=');
+    if (!username){
+        // invalid format for post args
+    }
+    *username = 0;
+    username += 1;  // Put username string to
+
+    char *password = strchr(sep1, '=');
+    if (!password){
+        // invalid format for post args
+    }
+    *password = 0;
+    password+=1;
+    ret[0] = strdup(username);
+    ret[1] = strdup(password);
+    free(postdata);
+    return ret;
+}
+
 int main(void){
+    printf("Content-Type:text/html%c%c\n", 13, 10);
+
+    char **userAndPass = parsePostRequest();
+    char *user = userAndPass[0];
+    char *password = userAndPass[1];
+
+    return 0;
+
     FILE *members = fopen(MEMBERS, "r");
-    char *name, *password;
+
     if (!members){
         // TODO: print error, members file not found
     }
@@ -31,14 +82,14 @@ int main(void){
     char line[1024];
     while (fgets(line, 1024, members)){
         char* tmp = strdup(line);
-        if (valid(name, password, tmp)){
+        if (valid(user, password, tmp)){
             FILE *loggedin = fopen(LOGGEDIN, "a+");
             FILE *cat = fopen(CATALOGUE, "r");
-            fprintf(loggedin, "%s\n", name);
+            fprintf(loggedin, "%s\n", user);
             char out[512];
             while (fgets(out, 512, cat)){
                 if (!strncmp(out, "{user}", 6))
-                    printf("<input type=\"hidden\" name=\"user\" value=\"%s\"/>", name);
+                    printf("<input type=\"hidden\" name=\"user\" value=\"%s\"/>", user);
                 else
                     printf("%s", out);
             }
