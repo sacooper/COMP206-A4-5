3910cd968a2391731222c514ed25a7b3ed762372
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2014-12-05 10:56:55 -0500

    Added error page to Login.c

--- cgi-bin/Login.c
+++ cgi-bin/Login.c
@@ -5,6 +5,23 @@
 #define MEMBERS "../data/Members.csv"
 #define LOGGEDIN "../data/LoggedIn.csv"
 #define CATALOGUE "../catalogue.html"
+#define ERROR "../error.html"
+
+void error(char *message){
+    FILE *errorpage = fopen(ERROR, "r");
+    if (!errorpage){
+        printf("ERROR\n");
+        exit (0);
+    }
+    char out[512];
+    while (fgets(out, 512, errorpage)){
+        if (strstr(out, "{errormessage}"))
+            printf("<h3 style=\"color:red\">%s</h3>\n", message);
+        else
+            printf("%s", out);
+    }
+    exit(0);
+}
 
 int valid(char *name, char *password, char *line){
     char *tok;
@@ -22,43 +39,38 @@ int valid(char *name, char *password, char *line){
 char** parsePostRequest(){
     char *len_ = getenv("CONTENT_LENGTH");
     if (!len_){
-        // invalid post request
-	exit(0);
+        error("Invalid Post Request");
     }
 
     long len = strtol(len_, NULL, 10);
     char *postdata = malloc(len + 1);
     if (!postdata) {
-        // TODO: error page...
-	exit(0);
+        error("Invalid Post Request");
     }
     fgets(postdata, len + 1, stdin);
 
     if (!strstr(postdata, "username") || !strstr(postdata, "password")){
-        // invalid form paramaters
-        exit(0);
+        error("Invalid Form Data");
     }
     char ** ret = malloc(2*sizeof(char*));
 
     char *sep1 = strchr(postdata, '&');
     if (!sep1){
 	exit(0);
-        // invalid input - not enough args
+        error("Invalid Form Data");
     }
     *sep1 = '\0';
     sep1 += 1;
     char *username = strchr(postdata, '=');
     if (!username){
-	exit(0);
-        // invalid format for post args
+        error("Invalid Form Data");
     }
     *username = 0;
-    username += 1;  // Put username string to
+    username += 1;
 
     char *password = strchr(sep1, '=');
     if (!password){
-	exit(0);
-        // invalid format for post args
+	       error("Invalid Form Data");
     }
     *password = 0;
     password+=1;
@@ -77,9 +89,7 @@ int main(void){
     FILE *members = fopen(MEMBERS, "r");
 
     if (!members){
-        // TODO: print error, members file not found
-        printf("ERROR!");
-        return 0;
+        error("Error: file not found");
     }
 
     char line[1024];
@@ -88,7 +98,9 @@ int main(void){
         if (valid(user, password, tmp)){
             FILE *loggedin = fopen(LOGGEDIN, "a+");
             FILE *cat = fopen(CATALOGUE, "r");
-
+            if (!loggedin || !cat){
+                error("File Not Found");
+            }
             fprintf(loggedin, "%s\n", user);
             char out[512];
             while (fgets(out, 512, cat)){
@@ -103,8 +115,7 @@ int main(void){
         free(tmp);
     }
 
-    // If we get here, it means incorrect login info
 
-    printf("Invalid login info");
+    error("Invalid login info");
     return 0;
 }
